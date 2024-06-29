ddefe4eddc5a94aae0e3c906fb0b786fdc930ce5
boburbekt <test@gmail.com>
2024-06-29 09:46:23 +0500

    mini

--- components/layouts/about.vue
+++ components/layouts/about.vue
@@ -1,30 +1,36 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
-    <div class="container mx-auto pt-3 pb-2">
-        <div class="flex justify-between items-start">
-            <div class="col">
-                <h2 class="title">
-                    Dastur haqida
-                </h2>
-                <p class="paragrf">
-                    INSELL Savdo Platformasi 15 turdagi savdo do'konlari ish jarayonlari uchun mahsus ishlab chiqilgan
-                    bo'lib, barcha yo'nalishlarni o'ziga hosligi hisobga olingan. Boshqa savdo dasturlaridan farqli
-                    INSELL Savdo Platformasi shablon emas.
-                </p>
-                <p class="paragrf mt-4"> Bundan tashqari, Dasturga qo’shimcha va o’zgartirishlar
-                    kiritish xizmati mavjud. Bu xizmatdan foydalanib, “MAHSUS DASTUR” ni premium foydalanuvchi sifati
-                    o’zingiz istaganingizdek buyurtma berishingiz mumkin.
-                </p>
-            </div>
-            <div class="col">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/uv0LekSBlAc?si=i94awOcogVsU5WLd"
-                    title="YouTube video player" frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-            </div>
-        </div>
+  <div class="container mx-auto pt-3 pb-2">
+    <div class="flex justify-between items-start">
+      <div class="col">
+        <h2 class="title">Dastur haqida</h2>
+        <p class="paragrf">
+          INSELL Savdo Platformasi 15 turdagi savdo do'konlari ish jarayonlari
+          uchun mahsus ishlab chiqilgan bo'lib, barcha yo'nalishlarni o'ziga
+          hosligi hisobga olingan. Boshqa savdo dasturlaridan farqli INSELL
+          Savdo Platformasi shablon emas.
+        </p>
+        <p class="paragrf mt-4">
+          Bundan tashqari, Dasturga qo’shimcha va o’zgartirishlar kiritish
+          xizmati mavjud. Bu xizmatdan foydalanib, “MAHSUS DASTUR” ni premium
+          foydalanuvchi sifati o’zingiz istaganingizdek buyurtma berishingiz
+          mumkin.
+        </p>
+      </div>
+      <div class="col">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/uv0LekSBlAc?si=i94awOcogVsU5WLd"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        ></iframe>
+      </div>
     </div>
+  </div>
 </template>
--- components/layouts/header.vue
+++ components/layouts/header.vue
@@ -1,16 +1,17 @@
 <template>
-    <div class="h-[500px] flex justify-between pt-5">
-        <div class="w-[50%]">
-            <h6 class="text-[25px]">
-                Insell savdo platformasi haqida -
-            </h6>
-            <h2 class="text-[30px] leading-0">
-               <span class="text-Primary"> INSELL Savdo Platformasi</span> 14 turdagi savdo do'konlari uchun mahsus ishlab chiqilgan bo'lib, barcha yo'nalishlarni o'ziga hosligi hisobga olingan. Boshqa savdo dasturlaridan farqli INSELL Savdo Platformasi shablon emas sizning barcha hohish istaklaringiz asosida ishlab chiqilgan.
-            </h2>
-        </div>
-        <div class="w-[40%]">
-            <NuxtImg src="/monoblock.jpg" height="400" />
-        </div>
-
+  <div class="h-[500px] flex justify-between pt-5">
+    <div class="w-[50%]">
+      <h6 class="text-[25px]">Insell savdo platformasi haqida -</h6>
+      <h2 class="text-[30px] leading-0">
+        <span class="text-Primary"> INSELL Savdo Platformasi</span> 14 turdagi
+        savdo do'konlari uchun mahsus ishlab chiqilgan bo'lib, barcha
+        yo'nalishlarni o'ziga hosligi hisobga olingan. Boshqa savdo
+        dasturlaridan farqli INSELL Savdo Platformasi shablon emas sizning
+        barcha hohish istaklaringiz asosida ishlab chiqilgan.
+      </h2>
     </div>
+    <div class="w-[40%]">
+      <NuxtImg src="/monoblock.jpg" height="400" />
+    </div>
+  </div>
 </template>
